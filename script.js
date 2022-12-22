0a267e4d27329762e30c69e517151f514a647d1c
Gustavo Medeiros <eugus03@gmail.com>
2022-12-21 21:44:31 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+function checkInputs() {
+  const usernameValue = username.value;
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  const passwordConfirmationValue = passwordConfirmation.value;
+}
